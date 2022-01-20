62bce5e5c70c2b3cbac11c46d3ba5ba7c6ede5e0
Dan Nguyen <60051771+nuhgooyin@users.noreply.github.com>
2022-01-19 21:29:17 -0500

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,285 @@
+/**
+ * Binary Tree Example Extended (Recursive)
+ * 
+ * @author Dan Nguyen
+ * @course ICS4U
+ * @date 2022/01/12
+ */
+public class BinaryTree {
+  boolean empty = true;
+  int data = 0;
+  BinaryTree left = null;
+  BinaryTree right = null;
+  boolean oneChild = false;
+
+  /**
+   * Constructor
+   * 
+   * @param key
+   */
+  public BinaryTree() {
+    this.data = 0;
+    this.empty = true;
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param key
+   */
+  public BinaryTree(int key) {
+    this.data = key;
+    this.empty = false;
+  }
+
+  /**
+   * Add a node to the tree
+   * 
+   * @param key
+   */
+  public void add(int key) {
+    // Is the tree empty
+    if (this.empty) {
+      this.data = key;
+      this.empty = false;
+    } else if (key < data) {
+      // Goes on the left side
+      if (this.left == null) {
+        this.left = new BinaryTree(key);
+      } else {
+        this.left.add(key);
+      }
+    } else {
+      // Goes on the right side
+      if (this.right == null) {
+        this.right = new BinaryTree(key);
+      } else {
+        this.right.add(key);
+      }
+    }
+  }
+
+  /**
+   * Inorder traversal prints nodes in sorted order
+   */
+  public String inOrderTraversal() {
+    String result = "";
+
+    // Check is it empty
+    if (this.empty) {
+      result = "Tree is empty";
+    } else {
+      // Left, Node, Right
+      if (this.left != null) {
+        result += this.left.inOrderTraversal();
+      }
+      result += this.data + " ";
+      if (this.right != null) {
+        result += this.right.inOrderTraversal();
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Reverse order traversal prints nodes in reversed sorted order
+   *
+   * @return String result
+   */
+  public String reverseOrderTraversal() {
+    String result = "";
+
+    // Check is it empty
+    if (this.empty) {
+      result = "Tree is empty";
+    } else {
+      // Right, Node, Left
+      if (this.right != null) {
+        result += this.right.inOrderTraversal();
+      }
+      result += this.data + " ";
+      if (this.left != null) {
+        result += this.left.inOrderTraversal();
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Count how many instances are in the tree
+   * 
+   * @param int key
+   * @return int counter
+   */
+  public int countInstance(int key) {
+
+    // Declare variable
+    int counter = 0;
+
+    // The tree is not empty
+    if (this.empty != true) {
+
+      // Is this the node?
+      if (this.data == key) {
+
+        // Increment
+        counter++;
+
+        // If right node exists
+        if (this.right != null) {
+
+          // Recursively keep counting to the right
+          counter += this.right.countInstance(key);
+        }
+      }
+
+      // Determine whether to recursively count the right or left side
+      else if (this.data > key && this.left != null) {
+        counter += this.left.countInstance(key);
+      } else if (this.data < key && this.right != null) {
+        counter += this.right.countInstance(key);
+      }
+    }
+    return counter;
+  }
+
+  /**
+   * Find the maximum value in subtree
+   * 
+   * @return int maxVal
+   */
+  public int findMaxVal() {
+
+    // Declare variable
+    BinaryTree node = this;
+
+    // While there are nodes to the right
+    while (node.right != null) {
+
+      // Move to right node
+      node = node.right;
+    }
+    return node.data;
+  }
+
+  /**
+   * Remove nodes from tree
+   * 
+   * @param int key
+   * @return void
+   */
+  public void remove(int key) {
+    this.oneChild = false;
+
+    // The node is not empty
+    if (this.empty != true) {
+
+      // Immediate node is the key?
+      if (this.data == key) {
+
+        // 2 child case
+        if (this.right != null && this.left != null) {
+
+          // Find maximum value in left subtree
+          int maxVal = this.left.findMaxVal();
+
+          // Set data
+          this.data = maxVal;
+
+          // Recursively remove the data set
+          this.left.remove(maxVal);
+        }
+
+        // 1 child case true
+        else if (this.right != null || this.left != null) {
+          this.oneChild = true;
+        }
+
+        // Leaf case set empty
+        else {
+          this.empty = true;
+        }
+      }
+
+      // Determine whether to recursively remove from the right or left side
+      else if (this.data > key && this.left != null) {
+        this.left.remove(key);
+
+        // One child case?
+        if (this.left.oneChild == true) {
+
+          // Is it on the left or right side?
+          if (this.left.left != null) {
+
+            // Reassign pointer
+            this.left = this.left.left;
+
+          } else if (this.left.right != null) {
+
+            // Reassign pointer
+            this.left = this.left.right;
+          }
+        }
+
+      } else if (this.data < key && this.right != null) {
+        this.right.remove(key);
+
+        // One child case?
+        if (this.right.oneChild == true) {
+
+          // Is it on the left or right side?
+          if (this.right.right != null) {
+
+            // Reassign pointer
+            this.right = this.right.right;
+
+          } else if (this.right.left != null) {
+
+            // Reassign pointer
+            this.right = this.right.left;
+          }
+        }
+      }
+
+      // Make sure there are no empty nodes on either side
+      if (this.left != null && this.left.empty == true) {
+        this.left = null;
+      } else if (this.right != null && this.right.empty == true) {
+        this.right = null;
+      }
+    }
+  }
+
+  /**
+   * Get the data of this node
+   * 
+   * @return int this.data
+   */
+  public int getData() {
+    return this.data;
+  }
+
+  /**
+   * Locate a value
+   * 
+   * @param key
+   * @return BinaryTree result
+   */
+  public BinaryTree find(int key) {
+    BinaryTree result = null;
+
+    if (this.empty != true) {
+      // Is this the node?
+      if (this.data == key) {
+        result = this;
+      } else if (this.data > key && this.left != null) {
+        // Search the left
+        result = this.left.find(key);
+      } else if (this.data < key && this.right != null) {
+        // Search the right
+        result = this.right.find(key);
+      }
+    }
+    return result;
+  }
+}
---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+/**
+ * Binary Tree Example Extended (Recursive)
+ * 
+ * @author Dan Nguyen
+ * @course ICS4U
+ * @date 2022/01/12
+ */
+class Main {
+  public static void main(String[] args) {
+    // Create a tree
+    BinaryTree theTree = new BinaryTree();
+
+    // Add a bunch of random data
+    // Random rand = new Random();
+    // System.out.print("Adding: ");
+    // for (int i = 0; i < 20; i++) {
+    //   int num = rand.nextInt(10);
+    //   System.out.print(num + " ");
+    //   theTree.add(num);
+    // }
+    // System.out.println();
+
+    // Add fixed following data in array
+    int[] numbers = { 100, 50, 25, 55, 54, 57, 56, 60, 150, 125, 175 };
+    System.out.print("Adding: ");
+    for (int i = 0; i <= numbers.length - 1; i++) {
+      theTree.add(numbers[i]);
+      System.out.print(numbers[i] + " ");
+    }
+    System.out.println();
+
+    // Let's find something
+    // System.out.println("Find");
+    // BinaryTree node = theTree.find(3);
+    // if (node == null) {
+    //   System.out.println("Not found");
+    // } else {
+    //   System.out.println("Found:" + node.getData());
+    // }
+
+    // Show the data in the tree
+    // System.out.println(theTree.inOrderTraversal());
+    // System.out.println(theTree.reverseOrderTraversal());
+
+    // Count how many instances are in the tree
+    // System.out.println("Count: " + theTree.countInstance(2));
+
+    // Remove certain data from the tree
+    System.out.println(theTree.inOrderTraversal());
+    theTree.remove(55);
+    System.out.println(theTree.inOrderTraversal());
+  }
+}
